36a3a2de367c85804831631c4911c797f43d9166
PengHian Ang <v_fphhang@tencent.com>
2023-10-18 02:21:22 +0800

    update

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "ami" {
+  description = "The ID of the AMI to use"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of instance to use"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key pair name"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "The path to the private key"
+  type        = string
+}
+
+variable "domain" {
+  description = "The domain name"
+  type        = string
+}
+
+variable "certificate_arn" {
+  description = "The certificate arn"
+  type        = string
+}
+
+variable "region" {
+  description = "The region"
+  type        = string
+}
+
+variable "environment" {
+  description = "crypto_terraform_tag"
+  type        = string
+}
